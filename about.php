72838374995d8cbc5db08ff9a893ef6961a8fd68
Ramkrishna732 <ramkrishnamaurya732@gmail.com>
2022-03-27 14:13:51 +0530

    first upload

---
+++ about.php
@@ -0,0 +1,2 @@
+<h2>About Us</h2>
+<h3>This is TCET's online notice board. Students can view all important notices regarding practical exams, oral exams, end-semester exams here.</h3>
---
+++ contact.php
@@ -0,0 +1,50 @@
+<h2> Give a Review</h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4">ENTER YOUR EMAIL</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">ENTER MOBILE NO.</div>
+		<div class="col-sm-5">
+		<input type="text" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">ENTER MESSEGE</div>
+		<div class="col-sm-5">
+		<input type="text" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Submit review" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h2>Contact us</h2>
+		
+		Name: prathamesh awati<br/>
+		Mobile: 9403298653<br/>
+		Email:awatiprathamesh8@gmail.com<br/>
+		
+		</div>
+	</div>
+	
+	
+</form>	
